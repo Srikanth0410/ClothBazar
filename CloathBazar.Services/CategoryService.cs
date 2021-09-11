7b26c6213d3f5e771cedd6b5bbbefde68ed9d674
Lenovo <Lenovo@LENOVO-PC>
2021-09-11 18:42:01 +0530

    2 Crud operations for Cateory table usin EF

---
+++ CloathBazar.Services/CategoryService.cs
@@ -0,0 +1,68 @@
+﻿using ClothBazar.DataBase;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloathBazar.Services
+{
+   public class CategoryService
+    {
+        public void SaveCategory(Category category)
+        {
+            //Here we are creating an object to the CbContext class (ClothBazar.DataBase) 
+            using (var context = new CBConttext())
+            {
+                //Need to add the entites into Category table.
+                context.Categories.Add(category);
+                //To saved the dataq in to table
+                context.SaveChanges();
+            }
+        }
+        // Here we will get list of categories so written type should be List<Category>
+        public List<Category> GetCategories()
+        {
+            using (var context = new CBConttext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+//Logic to get the particular ID
+        public Category GetCategory(int ID)
+        {
+            using (var context = new CBConttext())
+            {
+                return context.Categories.Find(ID);
+            }
+        }
+        //LOgic to update the ID
+        public void UpdateCategory(Category category)
+        {
+            using (var context = new CBConttext())
+            {
+                // we dont have update method in C#, so we need to tell to the EF like entities got modified.
+                // Then EF will update the changes in to the Database table.
+                context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }   
+
+
+        //Loic to Deete the record
+        public void DeletteCateory(int ID)
+        {
+            using (var context = new CBConttext())
+            {
+                var cateories = context.Categories.Find(ID);
+                //  Below one is for delte the record in EF style
+                // context.Entry(category).State = System.Data.Entity.EntityState.Deleted;
+                //  Below one is for delte the record in EF-2 style
+                context.Categories.Remove(cateories);
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ ClothBazar.Web/Controllers/CategoryController.cs
@@ -0,0 +1,70 @@
+﻿using CloathBazar.Services;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // Creating the object for the CategoryService class(ClothBazar.Services).
+        CategoryService categoryService = new CategoryService();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categories = categoryService.GetCategories();
+            //  above line of code and below line of code both are same only.
+            //  List<Category> categoriess = categoryService.GetCategories();
+            return View(categories);
+        }
+
+        // GET: Category
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryService.SaveCategory(category);
+            //  return View();
+            return RedirectToAction("Index");
+        }
+
+        // EDIT METHOD for Get and Update
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            var category = categoryService.GetCategory(Id);
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            categoryService.UpdateCategory(category);
+            // Once the changes are updated we are navigating to the Index action  method in the same controller
+            return RedirectToAction("Index");
+        }
+
+
+        //Delete Logic:
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            var category = categoryService.GetCategory(ID);
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {
+            // var category = categoryService.GetCategory(ID);
+            categoryService.DeletteCateory(category.ID);
+            return RedirectToAction("Index");
+        }
+    }
+}
